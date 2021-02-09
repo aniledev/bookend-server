cb99579d51ce49a485d8bf42466deab27624da91
aniledev <elinamcgill@gmail.com>
2021-02-09 02:00:47 -0500

    FEAT: write simple get route returning json data

--- src/app.js
+++ src/app.js
@@ -21,6 +21,10 @@ app.use(cors());
 app.use(express.json());
 
 //ROUTES
+app.get("/api/*", (req, res) => {
+  res.json({ ok: true });
+});
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
